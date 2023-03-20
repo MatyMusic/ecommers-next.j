072e45fae3e7692a61cbb2a87ba484a043d2dc27
maty <mtyg7702@gmail.com>
2023-03-20 13:03:39 +0200

    [field, meta]

--- pages/signin.js
+++ pages/signin.js
@@ -4,9 +4,30 @@ import styles from "../styles/signin.module.scss";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import Link from "next/link";
 import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import LoginInput from "@/components/inputs/loginInput";
+import { useState } from "react";
+
+const initialvalues = {
+  login_email: "",
+  login_password: "",
+};
 
 export default function signin() {
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  console.log(user);
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("נא למלא כתובת אימייל")
+      .email("נא להזין כתובת אימייל חוקית"),
+    login_password: Yup.string().required("הקלד סיסמא בבקשה"),
+  });
+
   return (
     <>
       <Header />
@@ -24,10 +45,30 @@ export default function signin() {
           <div className={styles.login__form}>
             <h1>כניסה</h1>
             <p>קבל גישה לאחד משירותי הקניות הטובים בעולם</p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
-                  <LoginInput icon="password" placeholder="כתובת אימייל" />
+                  <LoginInput
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="כתובת אימייל"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder=" סיסמא"
+                    onChange={handleChange}
+                  />
                 </Form>
               )}
             </Formik>
