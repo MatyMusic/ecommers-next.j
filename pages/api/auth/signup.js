ab5a7f08ca4679516e2f51b8a4f1927d3ce0100c
maty <mtyg7702@gmail.com>
2023-03-21 22:57:27 +0200

    JSONWEBTOKEN, BCRYPT MONGODB

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,10 @@
 import nc from "next-connect";
+import bcrypt from "bcrypt";
 import { validateEmail } from "../../../utils/validation";
 import db from "@/utils/db";
+import User from "@/models/User";
+import { createActivationToken } from "@/utils/tokens";
+
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -14,6 +18,32 @@ handler.post(async (req, res) => {
     if (!validateEmail(email)) {
       return res.status(400).json({ message: 'דוא"ל לא חוקי' });
     }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'דוא"ל זה קיים כבר במערכת' });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "סיסמא חייבת להיות לפחות שישה תווים" });
+    }
+
+    const bcryptPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: bcryptPassword });
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+
+    
+    
+
+
+
+    console.log(activation_token);
+    res.send(activation_token);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
