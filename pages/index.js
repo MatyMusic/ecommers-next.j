9158f7ee8c26d8cd5e5d0363b00746fd886ead4e
maty <mtyg7702@gmail.com>
2023-03-26 03:03:35 +0300

    ProductsSwiper

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,53 @@ const inter = Inter({ subsets: ["latin"] });
 import { useSession, signIn, signOut } from "next-auth/react";
 import Main from "@/components/home/main";
 import FlashDeals from "@/components/home/flashDeals";
+import Category from "@/components/home/category";
+import { women_accessories, women_dresses, women_shoes, women_swiper } from "@/data/home";
+import { useMediaQuery } from "react-responsive";
+import ProductsSwiper from "@/components/productsSwiper";
 
 export default function Home({ country }) {
   const { data: session } = useSession();
-  console.log(session);
+  const isMedium = useMediaQuery({ query: "(max-width:850px)" });
+  const isMobile = useMediaQuery({ query: "(max-width:550px)" });
 
   return (
     <>
       <Header country={country} />
       <div className={styles.home}>
         <div className={styles.container}>
-          <Main/>
-          <FlashDeals/>
+          <Main />
+          <FlashDeals />
+          <div className={styles.home__category}>
+            <Category
+              header="maty"
+              products={women_dresses}
+              background="#5a31f4"
+            />
+            {!isMedium && (
+              <Category
+                header="maty-2"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            {isMobile && (
+              <Category
+                header="maty-2"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            <Category
+              header="maty-3"
+              products={women_accessories}
+              background="#000"
+            />
+          </div>
+
+             <ProductsSwiper products={women_swiper}  />
+
+
         </div>
       </div>
       <Footer country={country} />
